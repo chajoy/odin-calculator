d6fedcac45c63196294769222ef4ffc3205baa01
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2024-11-08 17:21:19 +0000

    Add operate function to handle operations

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-const add = function (num1, num2)
-{
-    return num1 + num2;
+const add = function (num_1, num_2) {
+    return num_1 + num_2;
 }
 
-const subtract = function (num1, num2)
-{
-    return num1 - num2;
+const subtract = function (num_1, num_2) {
+    return num_1 - num_2;
 }
 
-const divide = function (num1, num2)
-{
-    return num1 / num2;
+const divide = function (num_1, num_2) {
+    return num_1 / num_2;
 }
 
-const multiply = function (num1, num2)
-{
-    return num1 * num2;
+const multiply = function (num_1, num_2) {
+    return num_1 * num_2;
+}
+
+const operate = function (operator, num_1, num_2) {
+    switch (operator) {
+        case `+`:
+            return add(num_1, num_2);
+            break;
+        case `-`:
+            return subtract(num_1, num_2);
+            break;
+        case `/`:
+            return divide(num_1, num_2);
+            break;
+        case `*`:
+            return multiply(num_1, num_2);
+            break;
+        default:
+            break;
+    }
 }
